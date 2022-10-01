8ad069ceac4f04e65c7fad8367a8783e2f4a9392
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-10-02 00:25:33 +0900

    feat. 로그인 페이지 구현
    - 사용하지 않는 MainActivity 관련 파일 제거
    - 로그인 페이지 layout 및 activity 작성

---
+++ app/src/main/java/com/example/a2022_mop/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2022_mop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstantState: Bundle?) {
+        super.onCreate(savedInstantState)
+        setContentView(R.layout.activity_login)
+    }
+}
