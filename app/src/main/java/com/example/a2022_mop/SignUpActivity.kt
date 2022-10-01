d027242c9e2616f242d54d3ae90d46676af25bb1
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-10-02 01:49:49 +0900

    feat. 회원가입 페이지 구현
    - 회원가입 관련 activity, layout 구현
    - manifests에 activity 추가

---
+++ app/src/main/java/com/example/a2022_mop/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2022_mop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity: AppCompatActivity() {
+    override fun onCreate(savedInstantState: Bundle?) {
+        super.onCreate(savedInstantState)
+        setContentView(R.layout.activity_signup)
+    }
+}
