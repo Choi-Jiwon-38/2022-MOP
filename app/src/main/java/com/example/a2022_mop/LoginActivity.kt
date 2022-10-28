aee077a1920e4116c21ab8cd91afc7b275e09ec1
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-10-28 23:19:07 +0900

    feat. 회원가입 이동 구현
    - 로그인 페이지에서 '회원가입' 텍스트 클릭시 회원가입 페이지로 이동하도록 Intent 설정

--- app/src/main/java/com/example/a2022_mop/LoginActivity.kt
+++ app/src/main/java/com/example/a2022_mop/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.a2022_mop
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
+    private lateinit var btnTextSignUp: TextView
+
     override fun onCreate(savedInstantState: Bundle?) {
         super.onCreate(savedInstantState)
         setContentView(R.layout.activity_login)
+
+        btnTextSignUp = findViewById(R.id.toSignUp)
+
+        btnTextSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
