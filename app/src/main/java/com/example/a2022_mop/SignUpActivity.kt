7198240271bc6c054e97a047a52e1e4ce1734911
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-10-29 13:42:47 +0900

    feat. 아이디 중복 체크 구현
    - Preferences에 저장하는 type을 SetString에서 String으로 변경(JsonObject 형태)
    - Preferences에 해당 key값(ID)의 존재유무에 따라 다른 Toast 메시지 출력

--- app/src/main/java/com/example/a2022_mop/SignUpActivity.kt
+++ app/src/main/java/com/example/a2022_mop/SignUpActivity.kt
@@ -4,7 +4,9 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
 
 class SignUpActivity: AppCompatActivity() {
     private lateinit var mEditID: EditText
@@ -13,7 +15,8 @@ class SignUpActivity: AppCompatActivity() {
     private lateinit var mEditAddress: EditText
     private lateinit var mEditPhone: EditText
 
-    private lateinit var btnSignUp: Button
+    private lateinit var btnCheck: Button   // 중복체크 버튼
+    private lateinit var btnSignUp: Button  // 회원가입 버튼
 
     // 회원 가입 시 사용될 정보
     private lateinit var newID: String
@@ -31,7 +34,9 @@ class SignUpActivity: AppCompatActivity() {
         mEditName = findViewById(R.id.info_name)
         mEditAddress = findViewById(R.id.info_address)
         mEditPhone = findViewById(R.id.info_phone)
+
         btnSignUp = findViewById(R.id.buttonSignUp)
+        btnCheck = findViewById(R.id.buttonCheck)
 
         val prefs: SharedPreferences = getSharedPreferences("member_info", 0)
         val editor: SharedPreferences.Editor = prefs.edit()
@@ -45,11 +50,30 @@ class SignUpActivity: AppCompatActivity() {
             userAddress = mEditAddress.text.toString()
             userPhone = mEditPhone.text.toString()
 
-            val info = setOf(newPW, userName, userAddress, userPhone) // 데이터는 'StringSet' 의 형태로 저장
+            val info_json = JSONObject()
+            info_json.put("password", newPW)
+            info_json.put("name", userName)
+            info_json.put("address", userAddress)
+            info_json.put("phone", userPhone)
 
-            editor.putStringSet(newID, info)
+            val info_string = info_json.toString()
+
+            editor.putString(newID, info_string)
             editor.apply()
 
+        }
+
+        btnCheck.setOnClickListener {
+            newID = mEditID.text.toString()
+
+
+            val check: String? = prefs.getString(newID, "none")
+
+            if (check == "none") {
+                Toast.makeText(this, "사용 가능한 아이디입니다!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "이미 존재하는 아이디입니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
