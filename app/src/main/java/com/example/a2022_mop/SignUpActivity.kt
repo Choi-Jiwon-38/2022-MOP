bac11127136371e29ae9a1c71300a8ced8024a4b
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-10-29 14:46:25 +0900

    feat. 회원가입 성공 조건 추가
    회원 가입 성공 조건에 아래 항목 추가
    - '동의함' RadioButton에 체크되어야 함
    - 중복체크 확인된 아이디와 같아야 함
    - 비밀번호를 포함한 사용자 개인정보가 모두 입력되어야 함
    - 성공 시, Toast 메시지와 함께 로그인 페이지로 돌아가도록 구현

--- app/src/main/java/com/example/a2022_mop/SignUpActivity.kt
+++ app/src/main/java/com/example/a2022_mop/SignUpActivity.kt
@@ -1,9 +1,11 @@
 package com.example.a2022_mop
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
@@ -14,9 +16,12 @@ class SignUpActivity: AppCompatActivity() {
     private lateinit var mEditName: EditText
     private lateinit var mEditAddress: EditText
     private lateinit var mEditPhone: EditText
+    private lateinit var mRadioAgree: RadioButton
 
     private lateinit var btnCheck: Button   // 중복체크 버튼
     private lateinit var btnSignUp: Button  // 회원가입 버튼
+
+    private var availableID: String = "" // 중복체크를 거친 아이디 (사용 가능한 아이디)
 
     // 회원 가입 시 사용될 정보
     private lateinit var newID: String
@@ -34,6 +39,7 @@ class SignUpActivity: AppCompatActivity() {
         mEditName = findViewById(R.id.info_name)
         mEditAddress = findViewById(R.id.info_address)
         mEditPhone = findViewById(R.id.info_phone)
+        mRadioAgree = findViewById(R.id.radioChecked)
 
         btnSignUp = findViewById(R.id.buttonSignUp)
         btnCheck = findViewById(R.id.buttonCheck)
@@ -43,34 +49,57 @@ class SignUpActivity: AppCompatActivity() {
 
         // 프리퍼런스를 이용하여 데이터 작성하기
         btnSignUp.setOnClickListener {
-
+            val isAgree = mRadioAgree.isChecked
             newID = mEditID.text.toString()
             newPW = mEditPW.text.toString()
             userName= mEditName.text.toString()
             userAddress = mEditAddress.text.toString()
             userPhone = mEditPhone.text.toString()
 
-            val info_json = JSONObject()
-            info_json.put("password", newPW)
-            info_json.put("name", userName)
-            info_json.put("address", userAddress)
-            info_json.put("phone", userPhone)
+            if (
+                isAgree
+                && (newID == availableID)
+                && newPW.isNotEmpty()
+                && userName.isNotEmpty()
+                && userAddress.isNotEmpty()
+                && userPhone.isNotEmpty()
+            ) {
+                val info_json = JSONObject()
+                info_json.put("password", newPW)
+                info_json.put("name", userName)
+                info_json.put("address", userAddress)
+                info_json.put("phone", userPhone)
 
-            val info_string = info_json.toString()
+                val info_string = info_json.toString()
 
-            editor.putString(newID, info_string)
-            editor.apply()
+                editor.putString(newID, info_string)
+                editor.apply()
+
+                Toast.makeText(this, "회원가입이 완료되었습니다!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, LoginActivity::class.java))
+
+            } else {
+                if (!isAgree && (newID != availableID)) { // 중복체크 X / 개인정보 수집 동의 X
+                    Toast.makeText(this, "개인정보 수집 동의 및 아이디 중복체크를 해주세요.", Toast.LENGTH_SHORT).show()
+                } else if (!isAgree) {
+                    Toast.makeText(this, "개인정보 수집에 동의해주세요.", Toast.LENGTH_SHORT).show()
+                } else if (newID != availableID) {
+                    Toast.makeText(this, "아이디 중복체크를 해주세요.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "모든 정보를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
 
         }
 
         btnCheck.setOnClickListener {
             newID = mEditID.text.toString()
 
-
             val check: String? = prefs.getString(newID, "none")
 
             if (check == "none") {
                 Toast.makeText(this, "사용 가능한 아이디입니다!", Toast.LENGTH_SHORT).show()
+                availableID = newID
             } else {
                 Toast.makeText(this, "이미 존재하는 아이디입니다.", Toast.LENGTH_SHORT).show()
             }
